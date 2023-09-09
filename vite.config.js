43b824c1b9997f15ffc48cb1670d5fb10ee9d4e4
gdurro <grzegorzchra@gmail.com>
2023-09-09 23:43:52 +0200

    update: base route

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fistaszek',
+  base: "/fistach",
   server: {
     host: true
   },
